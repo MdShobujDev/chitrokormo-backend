41a40d38ee9c4428430a622b58c0c4130816474b
Md Shobuj Mia <md.shobuj.developer@gmail.com>
2025-03-16 21:44:39 +0600

    feat: add cors setup at middleware file

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -27,7 +27,15 @@ export default [
       },
     },
   },
-  "strapi::cors",
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      origin: ["https://chitrokormo.vercel.app/", "http://localhost:3000"], // Add frontend URLs
+      headers: "*", // Allow all headers
+      methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"], // Allowed request methods
+    },
+  },
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
